b5da9b9d8b7d9ae3fa097c11c9e74c247741d9bb
张庆华 <qinghua.zhang@topevery.net>
2020-07-16 16:01:39 +0800

    打印

--- src/main/java/com/zqh/springboot/demo/controller/IndexController.java
+++ src/main/java/com/zqh/springboot/demo/controller/IndexController.java
@@ -1,8 +1,11 @@
 package com.zqh.springboot.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @description:
@@ -14,8 +17,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/index")
 public class IndexController {
 
+    @Autowired
+    HttpServletRequest httpServletRequest;
+
     @GetMapping
     public String index() {
+        System.out.println("访问了index接口");
         return "这是index接口";
     }
 
